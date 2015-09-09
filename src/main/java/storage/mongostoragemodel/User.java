44a3d7606b9a384a712105c14b1e5c7caa028249
Joe <jrossi6@gatech.edu>
2015-09-09 12:14:35 -0400

    added friends list to User class in mongodbmodel

--- src/main/java/storage/mongostoragemodel/User.java
+++ src/main/java/storage/mongostoragemodel/User.java
@@ -1,5 +1,8 @@
 package storage.mongostoragemodel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Joe on 9/7/2015.
  */
@@ -8,12 +11,14 @@ public class User {
     private String hashedPassword;
     private String firstName;
     private String lastName;
+    private List<User> friends;
 
     public User(String _id, String hashedPassword, String firstName, String lastName) {
         this._id = _id;
         this.hashedPassword = hashedPassword;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.friends = new ArrayList<User>();
     }
 
     public String get_id() {
@@ -46,5 +51,17 @@ public class User {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public void addFriend(User u) {
+        friends.add(u);
+    }
+
+    public void removeFriend(User u) {
+        friends.remove(u);
+    }
+
+    public boolean isFriend(User u) {
+        return friends.contains(u);
     }
 }
