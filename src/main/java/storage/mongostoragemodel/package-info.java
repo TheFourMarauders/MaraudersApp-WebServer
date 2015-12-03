408cb3234fa67444514f243c47b79647087ef8fc
Joe <jrossi6@gatech.edu>
2015-12-03 15:47:30 -0500

    included images in javadoc pages

--- src/main/java/storage/mongostoragemodel/package-info.java
+++ src/main/java/storage/mongostoragemodel/package-info.java
@@ -1,5 +1,7 @@
 /**
  * Defines the schema for mongo storage, similar to storage.datatypes,
  * but could in theory be different.
+ *
+ * <img src="{@docRoot}/images/storage_model.png">
  */
 package storage.mongostoragemodel;
